7f030dff3738a361cdfda1b0e2237dcc8a08ab87
saepudinjeh <saepudinjeh@gmail.com>
2021-10-07 16:14:51 +0700

    update landing page, add manifest and favicon.png

--- src/scripts/views/pages/frontPage.js
+++ src/scripts/views/pages/frontPage.js
@@ -2,32 +2,39 @@ const FrontPage = {
   async render() {
     return `
       <section class="text-center">
-        Dashboard Admin
+        <a href="/" class="flex justify-center items-center mt-4">
+          <div class="w-12 h-12">
+            <img src="lumintu.svg">
+          </div>
+          <h1 class="ml-2 font-medium">
+            Lumintu Event
+          </h1>
+        </a>
 
 
         <div class="grid sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-4 items-center w-full mt-9">
-          <button name="button check in" class="shadow-md bg-gradient-to-b from-green-300 to-blue-300 hover:bg-gradient-to-b hover:from-green-400 hover:to-blue-400 rounded-md h-40 hover:shadow-lg text-blue-900">
+          <button name="button check in" class="shadow-md bg-white rounded-lg h-40 hover:shadow-lg text-gray-700">
             <a href="/">
               <span class="iconify mx-auto text-4xl" data-icon="la:user-check"></span>
               <span class="">Check In</span>
             </a>
           </button>
           
-          <button name="button check point" class="shadow-md bg-gradient-to-b from-yellow-300 to-yellow-400 hover:bg-gradient-to-b hover:from-yellow-400 hover:to-yellow-500 rounded-md h-40 hover:shadow-lg text-yellow-800">
+          <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 h-40 hover:shadow-lg text-yellow-800">
             <a href="/">
               <span class="iconify mx-auto text-3xl" data-icon="bi:calendar2-check"></span>
               <span class="">Check Point</span>
             </a>
           </button>
 
-          <button name="button check point" class="shadow-md sm:bg-gradient-to-t sm:from-yellow-300 sm:to-yellow-400 sm:hover:bg-gradient-to-b sm:hover:from-yellow-400 sm:hover:to-yellow-500 hover:bg-gradient-to-b hover:from-green-400 hover:to-blue-400 bg-gradient-to-t from-green-300 to-blue-300 rounded-md h-40 hover:shadow-lg sm:text-yellow-800 text-blue-900">
+          <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 hover:shadow-lg sm:text-yellow-800 text-gray-700">
             <a href="/">
               <span class="iconify mx-auto text-5xl" data-icon="ph:users-four-light"></span>
               <span class="">Data Peserta</span>
             </a>
           </button>
 
-          <button name="button check point" class="shadow-md sm:bg-gradient-to-t sm:from-green-300 sm:to-blue-300 sm:hover:bg-gradient-to-b sm:hover:from-green-400 sm:hover:to-blue-400 hover:bg-gradient-to-b hover:from-yellow-400 hover:to-yellow-500 bg-gradient-to-t from-yellow-300 to-yellow-400 rounded-md h-40 hover:shadow-lg sm:text-blue-900 text-yellow-800">
+          <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 h-40 hover:shadow-lg text-yellow-800 sm:text-gray-700">
             <a href="/">
               <span class="iconify mx-auto text-5xl" data-icon="healthicons:i-schedule-school-date-time-outline"></span>
               <span class="">Session</span>
@@ -35,7 +42,14 @@ const FrontPage = {
           </button>
         </div>
 
-        <h5 class="mt-10 text-xs text-gray-600">Lumintu Logic</h5>
+        <div class="flex justify-center items-center mt-14 text-xs text-gray-600">
+          <p>
+          © 2021 Lumintu Event |
+          </p>
+          <a href="/" class="font-bold text-yellow-800 ml-1">
+            Lumintu Logic
+          </a> 
+        </div>
       </section>
     `;
   },

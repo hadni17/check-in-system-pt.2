f4c02f44a5f3e1ca5deb84b6cab747183fec0e8a
saepudinjeh <saepudinjeh@gmail.com>
2021-10-11 13:37:59 +0700

    add scanPage

--- src/scripts/views/pages/frontPage.js
+++ src/scripts/views/pages/frontPage.js
@@ -6,7 +6,7 @@ const FrontPage = {
           <div class="w-12 h-12">
             <img src="lumintu.svg">
           </div>
-          <h1 class="ml-2 font-medium">
+          <h1 class="ml-2 font-medium text-gray-800 sm:text-lg text-base" style="font-family: 'Pacifico', cursive;">
             Lumintu Event
           </h1>
         </a>
@@ -14,30 +14,30 @@ const FrontPage = {
 
         <div class="grid sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-4 items-center w-full mt-9">
           <button name="button check in" class="shadow-md bg-white rounded-lg h-40 hover:shadow-lg text-gray-700">
-            <a href="/">
+            <a href="/#/scan">
               <span class="iconify mx-auto text-4xl" data-icon="la:user-check"></span>
-              <span class="">Check In</span>
+              <span class="">Scan</span>
             </a>
           </button>
-          
+
           <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 h-40 hover:shadow-lg text-yellow-800">
             <a href="/">
-              <span class="iconify mx-auto text-3xl" data-icon="bi:calendar2-check"></span>
-              <span class="">Check Point</span>
+              <span class="iconify mx-auto text-5xl" data-icon="ph:users-four-light"></span>
+              <span class="">Participant</span>
             </a>
           </button>
 
           <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 hover:shadow-lg sm:text-yellow-800 text-gray-700">
             <a href="/">
-              <span class="iconify mx-auto text-5xl" data-icon="ph:users-four-light"></span>
-              <span class="">Data Peserta</span>
+              <span class="iconify mx-auto text-3xl" data-icon="bi:calendar2-check"></span>
+              <span class="">Session</span>
             </a>
           </button>
 
           <button name="button check point" class="shadow-md shadow-md bg-white rounded-lg h-40 h-40 hover:shadow-lg text-yellow-800 sm:text-gray-700">
             <a href="/">
               <span class="iconify mx-auto text-5xl" data-icon="healthicons:i-schedule-school-date-time-outline"></span>
-              <span class="">Session</span>
+              <span class="">Merchandise</span>
             </a>
           </button>
         </div>
@@ -48,7 +48,7 @@ const FrontPage = {
           </p>
           <a href="/" class="font-bold text-yellow-800 ml-1">
             Lumintu Logic
-          </a> 
+          </a>
         </div>
       </section>
     `;
@@ -60,4 +60,3 @@ const FrontPage = {
 };
 
 export default FrontPage;
-

52c55c25c90dc78c04a9bb3975caa3c735b57f8e
saepudinjeh <saepudinjeh@gmail.com>
2021-10-29 13:39:25 +0700

    update location href scan page

--- src/scripts/views/pages/scanPage.js
+++ src/scripts/views/pages/scanPage.js
@@ -17,14 +17,14 @@ const scanPage = {
           </div>
 
          <div class="mt-8">
-            <div class="bg-white flex w-4/5 items-center rounded-xl shadow-xl mx-auto">
+            <form class="bg-white flex w-4/5 items-center rounded-xl shadow-xl mx-auto">
                 <input class="rounded-xl w-full py-1 px-6 text-gray-700 leading-tight focus:outline-none" id="cam-qr-result" type="text" placeholder="Search Your Participant ID">
               <div class="p-4">
-                <button class="text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+                <button type="submit" class="text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
                   <span class="iconify text-2xl text-gray-700" data-icon="bx:bx-search-alt"></span>
                 </button>
               </div>
-            </div>
+            </form>
          </div>
 
       </section>
@@ -36,7 +36,6 @@ const scanPage = {
     async function onScanSuccess(decodedText, decodedResult) {
       // handle the scanned code as you like, for example:
       window.location.replace(`/#/participant/${decodedText}`);
-      console.log(`Code matched = ${decodedText}`, decodedResult);
     }
 
     async function onScanFailure(error) {
@@ -49,6 +48,8 @@ const scanPage = {
       "preview", { fps: 10, qrbox: 200 }, /* verbose= */ false);
 
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+    // const getValue = 
 
   }
 };
